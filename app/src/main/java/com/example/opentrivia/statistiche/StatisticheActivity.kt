9998d4e5bec7c4ffe66f5fb72c93481b9d2721fe
domenico1597 <domenico.ciavarella15@gmail.com>
2023-11-04 12:34:25 +0100

    domenico 04/11

---
+++ app/src/main/java/com/example/opentrivia/statistiche/StatisticheActivity.kt
@@ -0,0 +1,15 @@
+package com.example.opentrivia.statistiche
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.opentrivia.R
+
+class StatisticheActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stat)
+
+
+    }
+}
